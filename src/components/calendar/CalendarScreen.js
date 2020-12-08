b1d1fe517c6b98c4afab2143abd2af31e117f6ca
oscar <oscarfiend@gmail.com>
2020-12-07 22:19:16 -0500

    calendario corriendo en espanol y con estilos propios

--- src/components/calendar/CalendarScreen.js
+++ src/components/calendar/CalendarScreen.js
@@ -1,9 +1,48 @@
 import React from 'react'
+import Navbar from '../ui/Navbar'
+import {Calendar,momentLocalizer} from 'react-big-calendar'
+import moment from 'moment'
+import 'moment/locale/es'
+import 'react-big-calendar/lib/css/react-big-calendar.css'
+import {messages} from '../../helpers/calendar-messages'
+
+moment.locale('es')
+
+const localizer = momentLocalizer(moment) 
 
 const CalendarScreen = () => {
+
+    const events=[{
+        title:'Cumpleanos del jefe',
+        start:moment().add(-10,'hours').toDate(),
+        end:moment().add(-7,'hours').toDate(),
+        bgcolor:"#7e3434"
+    }]
+
+    const eventStyleGetter=(event,  start, end, isSelected)=>{
+        const style={
+            backgroundColor:'#367cf7',
+            borderRadius:'0px',
+            opacity:0.8,
+            display:'block',
+            color:'white'
+        }
+        return{
+            style
+        }
+    }
+
     return (
-        <div>
-            <h1>Calendar Screen</h1>
+        <div className="calendar-screen">
+            <Navbar/>
+            <Calendar
+            localizer={localizer}
+            events={events}
+            startAccessor="start"
+            endAccessor="end"
+            messages={messages}
+            eventPropGetter={eventStyleGetter}
+            />
         </div>
     )
 }
